91f9ccd4698b7ff63386ae1674105f0d6a8d1b03
sachin supunthaka <sachinsupunthaka96@gmail.com>
2025-09-07 22:07:53 +0530

    chor: common types add and updating service logic

--- apps/turborepo-nestjs/src/products/products.controller.ts
+++ apps/turborepo-nestjs/src/products/products.controller.ts
@@ -1,10 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProductsService } from './products.service';
+
+import { CreateProductRequest } from '@repo/types';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  async createProducrt(@Body() createProductRequest) {}
+  async createProducrt(@Body() createProductRequest: CreateProductRequest) {
+    return this.productsService.createProduct(createProductRequest);
+  }
+
+  @Get()
+  async getProducts() {
+    return this.productsService.getProducts();
+  }
 }
--- apps/turborepo-nestjs/src/products/products.service.ts
+++ apps/turborepo-nestjs/src/products/products.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { CreateProductRequest } from '@repo/types';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  createProduct(createProductRequest: CreateProductRequest) {}
+
+  getProducts() {}
+}
